f0178bce66300e1a60a0075488996a9bfe753b0a
maningreen <flibbers@protonmail.com>
2024-12-27 10:59:53 -0700

    made camera shake better

--- scripts/camera.cs
+++ scripts/camera.cs
@@ -4,12 +4,13 @@ using System;
 public partial class camera : Camera2D
 {
   [Export]
-  public float smoothingRatio { get; set; } = .1f;
-  float mag;
+  public float dampening { get; set; } = .1f;
+  float mag = .1f;
   float phase = 0;
   float jitness;
   Vector2 shakeVector = Vector2.Zero;
   Vector2 endPos = Vector2.Zero;
+  Random rand = new Random();
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
@@ -19,23 +20,29 @@ public partial class camera : Camera2D
   public override void _Process(double delta)
   {
     if (Input.IsActionJustPressed("space"))
-      applyScreenShake(15, 0, 9);
+      applyScreenShake(9, 0, 1);
     //i was gonna do some trig stuff
     //but this is too dumb to ignore
     //(its 1:45 am btw)
-    Random rand = new Random();
-    Vector2 randBox = new Vector2(rand.Next(-(int)jitness, (int)jitness), rand.Next(-(int)jitness, (int)jitness));  //this will be copied
-    shakeVector += randBox * mag;
-    randBox = new Vector2(rand.Next(-(int)jitness, (int)jitness), rand.Next(-(int)jitness, (int)jitness));  //this will be copied
-    Position += endPos - (Position + randBox + shakeVector);
-    jitness += -jitness * smoothingRatio * (float)delta;
-    mag += -mag * smoothingRatio * (float)delta;
+    Vector2 randBox = new Vector2(randSingle(rand, jitness), randSingle(rand, jitness));
+    shakeVector += randBox;
+    randBox = new Vector2(randSingle(rand, jitness), randSingle(rand, jitness));
+
+    Position = endPos + (randBox + shakeVector) * mag;
+    jitness += -jitness * dampening * (float)delta;
+    mag += -mag * dampening * (float)delta;
   }
 
   public void applyScreenShake(float magnitude, float dirRads, int jitterness)
   {
     mag = magnitude;
-    shakeVector = Vector2.FromAngle(dirRads) * mag;  //thers a function for that :O
+    shakeVector = Vector2.FromAngle(dirRads);  //thers a function for that :O
     jitness = jitterness;
+  }
+
+  public float randSingle(Random rand, float inV)
+  {
+    float scalar = (rand.NextSingle() - .5f) * 2.0f;
+    return scalar * inV;
   }
 }
