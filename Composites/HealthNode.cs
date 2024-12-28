433c79ab74851be97b85def94518d02207cdedbc
maningreen <flibbers@protonmail.com>
2024-12-28 09:21:14 -0700

    added startHealth parameter and fixed some formatting (lsp did that for me)

--- Composites/HealthNode.cs
+++ Composites/HealthNode.cs
@@ -2,35 +2,39 @@ using Godot;
 
 public partial class HealthNode : Node
 {
-	[Signal]
-	public delegate void DeadEventHandler();
-
-	[Export]
-	public float MaxHealth
-	{
-		get => maxHealth;
-		set => maxHealth = Mathf.Max(1.0f, value);
-	}
-
-	public float Health
-	{
-		get => health;
-        set
-        {
-            health = Mathf.Clamp(value, 0.0f, maxHealth);
-            if (health <= 0.0f)
-            {
-                EmitSignal(nameof(Dead));
-            }
-        }
-	}
-
-	private float maxHealth;
-	private float health;
+  [Signal]
+  public delegate void DeadEventHandler();
 
 
-    public override void _Ready()
+  [Export]
+  public float MaxHealth
+  {
+    get => maxHealth;
+    set => maxHealth = Mathf.Max(1.0f, value);
+  }
+
+  [Export]
+  //if its negative it'll start at maxHealth
+  public float startHealth = -1;
+
+  public float Health
+  {
+    get => health;
+    set
     {
-        Health = MaxHealth;
+      health = Mathf.Clamp(value, 0.0f, maxHealth);
+      if (health <= 0.0f)
+      {
+        EmitSignal(nameof(Dead));
+      }
     }
+  }
+
+  private float maxHealth;
+  private float health;
+
+  public override void _Ready()
+  {
+    Health = startHealth < 0 ? maxHealth : startHealth;
+  }
 }
