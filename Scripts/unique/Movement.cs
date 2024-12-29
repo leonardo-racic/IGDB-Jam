0c09714106f7c38734e1646f4f7c013725994dd9
maningreen <flibbers@protonmail.com>
2024-12-28 18:27:20 -0700

    minor refactoring

--- Scripts/unique/Movement.cs
+++ Scripts/unique/Movement.cs
@@ -15,41 +15,36 @@ public partial class Movement : Node
   [Export]
   public int stepCount = 20;
 
-  private Vector2 lastInputDirection = Vector2.Right;
+  private Vector2 lastValidInputDir = Vector2.Zero;
 
   public override void _PhysicsProcess(double delta)
   {
     if (plr.Attacking)
-    {
       handleTackleMovement(delta);
-    } 
     else
-    {
       handleDefaultMovement(delta);
-    }
     move(plr, stepCount, bounceFactor);
   }
 
   private void handleDefaultMovement(double delta)
   {
     Vector2 inputDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+    plr.Velocity += inputDirection * (float)(SPEED * delta);
+    plr.Velocity *= FRICTION;
     if (inputDirection != Vector2.Zero)
     {
-      lastInputDirection = inputDirection;
+      lastValidInputDir = inputDirection;
       if (inputDirection.X != 0.0f)
-        plr.Pivot.Scale = new Vector2(2*(inputDirection.X > 0.0f ? 1 : 0) - 1, 1);
-      plr.Velocity = inputDirection * (float)(SPEED * delta);
+        plr.Pivot.Scale = new Vector2(2 * (inputDirection.X > 0.0f ? 1 : 0) - 1, 1);
       plr.PlayState("move");
     }
     else
-    {
-      plr.Velocity *= FRICTION * (float)delta;
       plr.PlayState("idle");
-    }
   }
 
-  private void handleTackleMovement(double delta) {
-    plr.Velocity = lastInputDirection * (float)(TACKLE_SPEED * delta);
+  private void handleTackleMovement(double delta)
+  {
+    plr.Velocity = lastValidInputDir * (float)(TACKLE_SPEED * delta);
     plr.PlayState("tackle");
   }
 
@@ -61,10 +56,8 @@ public partial class Movement : Node
     {  //save on memory by avoiding a for (we save 4 bytes, less if we use a short)
       KinematicCollision2D col = body.MoveAndCollide(incVel, true);
       if (col == null)
-      {
         //we're in the clear
         body.Position += incVel;
-      }
       else
       {
         //WE HIT THE SECOND TOWER
