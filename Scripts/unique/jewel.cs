b46a8ee731e32a10b6ada17fc5476498fb03a70a
maningreen <flibbers@protonmail.com>
2024-12-30 15:15:14 -0700

    work on collection

--- Scripts/unique/jewel.cs
+++ Scripts/unique/jewel.cs
@@ -3,6 +3,13 @@ using System;
 
 public partial class jewel : CharacterBody2D
 {
+  [Export]
+  public float dampening = .04f;
+  [Export]
+  public float pushFactor = 1.0f;
+  bool collecting = false;
+  Area2D plr = null;
+  float t = 0.0f;
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
@@ -11,11 +18,26 @@ public partial class jewel : CharacterBody2D
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(double delta)
   {
+    if (collecting)
+    {
+      Velocity += GlobalPosition.Lerp(plr.GlobalPosition, t) - GlobalPosition;
+      GD.Print(t);
+      t += (float)delta;
+      t = t > 1 ? 1 : t;
+    }
+  }
+
+  public override void _PhysicsProcess(double delta)
+  {
+    if (MoveAndSlide())
+      CallDeferred("queue_free");
   }
 
   public void areaEntered(Area2D area)
   {
-    QueueFree();
- 
+    if (!collecting)
+      Velocity += (Position - area.Position) * pushFactor;
+    collecting = true;
+    plr = area;
   }
 }
