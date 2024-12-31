f3a2d2e67b3c462bbbb0a8683ca64881bfb2566c
leonardo-racic <leonardo.racic.travail@gmail.com>
2024-12-31 20:28:00 +0100

    Simplifications
    - there's the error to correct still...

--- Scripts/unique/Enemy.cs
+++ Scripts/unique/Enemy.cs
@@ -8,8 +8,6 @@ public partial class Enemy : CharacterBody2D
   [Export] // used by EnemyMovment
   public NavigationAgent2D Nav;
   [Export]
-  public float TargetDistance = 200.0f;
-  [Export]
   public Node2D Pivot;
   [Export]
   public int stepCount = 3;
@@ -28,7 +26,6 @@ public partial class Enemy : CharacterBody2D
 
   MainScene main;
 
-  private bool fleeing = false;
   private Node2D pl = null;
   private int PlayerCollisionID = 2;
 
@@ -42,46 +39,10 @@ public partial class Enemy : CharacterBody2D
     main = GetTree().CurrentScene as MainScene;
   }
 
-  public override void _Process(double delta)
-  {
-    /*if (fleeing && pl != null)
-    {
-      //points away
-      Vector2 away = GlobalPosition - pl.GlobalPosition; //(away)
-      //(not towards; away)
-      float mag = TargetDistance - away.Length();
-      Velocity = away.Normalized() * mag * (float)delta;
-      Pivot.Scale = new Vector2(Velocity.X > 0.0f ? 1 : -1, 1);
-    }
-    else
-      Velocity = Vector2.Zero;*/
-  }
-
   public override void _PhysicsProcess(double delta)
   {
     movment.move(stepCount, this);
   }
-
-  void objectIsInView(Area2D area)
-  {
-    if (!area.GetCollisionLayerValue(PlayerCollisionID))
-      return;
-    ShapeCast2D ray = GetNode<ShapeCast2D>("Ray");
-    ray.TargetPosition = area.GlobalPosition - ray.GlobalPosition;
-    ray.ForceShapecastUpdate();
-    if (ray.GetCollisionCount() == 0)
-      return;
-    GodotObject col = ray.GetCollider(0);
-    fleeing = col.GetInstanceId() == area.GetInstanceId();
-    if (fleeing)
-      pl = area;
-  }
-
-  void objectLeftView(Area2D area)
-  {
-    fleeing = area.GetCollisionLayerValue(PlayerCollisionID) ? false : fleeing;
-  }
-
   public void onDead()
   {
     //WE SPAWN THEM DOUBLOONS
--- Scripts/unique/MainScene.cs
+++ Scripts/unique/MainScene.cs
@@ -17,12 +17,27 @@ public partial class MainScene : Node2D
 
     public override void _PhysicsProcess(double delta)
     {
-		if (plrObstacle == null) 
+		if (plrObstacle == null || NavRegion.IsBaking()) 
 			return;
 		plrObstacle.GlobalPosition = plr.GlobalPosition;
-		if (!NavRegion.IsBaking())
-			NavRegion.BakeNavigationPolygon();
+		handleObstacles();
+		NavRegion.BakeNavigationPolygon(false);
     }
+
+	// Used in order to  avoid the "Navigation map synchronisation error" coming up due to two obstacles overlapping...
+	public void handleObstacles() {
+		// WIP
+		foreach (Node node in NavRegion.GetChildren())
+		{
+			NavigationObstacle2D obstacle = node as NavigationObstacle2D;
+			if (obstacle == plrObstacle)
+			    continue;
+			else if (plrObstacle.GlobalPosition.DistanceTo(obstacle.GlobalPosition) > plrObstacle.Radius)
+				obstacle.AffectNavigationMesh = true;
+			else
+				obstacle.AffectNavigationMesh = false;
+		}
+	}
 
     public void OnRobberEvaded(Enemy robber)
 	{
