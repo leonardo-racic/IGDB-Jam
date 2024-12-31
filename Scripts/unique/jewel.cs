2221b89cb1cde1933399a6eac75bbd3f546a26ce
maningreen <flibbers@protonmail.com>
2024-12-30 19:12:58 -0700

    finished

--- Scripts/unique/jewel.cs
+++ Scripts/unique/jewel.cs
@@ -4,12 +4,14 @@ using System;
 public partial class jewel : CharacterBody2D
 {
   [Export]
-  public float dampening = .04f;
+  public float firstDistance = 100.0f;
   [Export]
-  public float pushFactor = 1.0f;
-  bool collecting = false;
+  public float acceleration = 50.0f;
+  bool start = false;
   Area2D plr = null;
-  float t = 0.0f;
+  float distance = 0.0f;
+  float dVel = 0.0f;
+  float angle = 0.0f;
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
@@ -18,26 +20,42 @@ public partial class jewel : CharacterBody2D
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(double delta)
   {
-    if (collecting)
+    if (plr != null)
     {
-      Velocity += GlobalPosition.Lerp(plr.GlobalPosition, t) - GlobalPosition;
-      GD.Print(t);
-      t += (float)delta;
-      t = t > 1 ? 1 : t;
+      angle = plr.GlobalPosition.AngleToPoint(GlobalPosition);
+      if (start)
+      {
+        distance = firstDistance;
+        if (distance >= firstDistance)
+          start = !start;
+      }
+      else
+      {
+        dVel -= acceleration;
+      }
+      distance += dVel;
+      setPositionAngle();
+      Position += plr.GlobalPosition;
+      GD.Print(distance);
     }
   }
 
   public override void _PhysicsProcess(double delta)
   {
-    if (MoveAndSlide())
+    if (distance <= 0 && plr != null)
       CallDeferred("queue_free");
   }
 
   public void areaEntered(Area2D area)
   {
-    if (!collecting)
-      Velocity += (Position - area.Position) * pushFactor;
-    collecting = true;
+    angle = area.GlobalPosition.AngleToPoint(GlobalPosition);
+    distance = (area.GlobalPosition - GlobalPosition).Length();
+    start = true;
     plr = area;
+  }
+
+  public void setPositionAngle()
+  {
+    GlobalPosition = Vector2.FromAngle(angle) * distance;
   }
 }
