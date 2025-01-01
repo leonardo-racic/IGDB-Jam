0e8e14d60ec69feff6ecb1d25abf447e225887dc
maningreen <flibbers@protonmail.com>
2024-12-31 18:22:36 -0700

    did some funky stuff now just more to do to scene

--- Scripts/unique/destructableObstacle.cs
+++ Scripts/unique/destructableObstacle.cs
@@ -1,42 +1,39 @@
 using Godot;
 using System;
 
-public partial class destructableObstacle : StaticBody2D
+public partial class destructableObstacle : Sprite2D
 {
   [Export]
-  public CircleShape2D collisionShape;
+  private NavigationObstacle2D navOb;
   [Export]
-  public Rect2 textureOffset;
+  public NavigationRegion2D navMesh;
+
   [Export]
   public float health;
   [Export]
-  public HealthNode HealthNode;
+  private HealthNode healthNode;
 
-  [Export]
-  private CollisionShape2D[] collisionShapes;
-  [Export]
-  private Sprite2D spriteNode;
-  [Export]
-  private NavigationObstacle2D navOb;
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
-    foreach (CollisionShape2D item in collisionShapes)
-    {
-      item.Shape = collisionShape as Shape2D;
-    }
-    navOb.Radius = collisionShape.Radius;
-    HealthNode.startHealth = health;
-    spriteNode.RegionRect = textureOffset;
-    HealthNode.Dead += () => onDead();
+    healthNode.MaxHealth = health;
+    healthNode.Health = health;
+    RemoveChild(navOb);
+    navMesh.AddChild(navOb);
+    GD.Print(navOb.GetPath());
+    navOb.QueueFree();
+    healthNode.Dead += () => onDead();
   }
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(double delta)
   {
+
   }
+
   void onDead()
   {
+    navOb.QueueFree();
     QueueFree();
   }
 }
