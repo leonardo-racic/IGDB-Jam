e1339aff95e5eeb7a2ae9a7f0c6dab2872b43a27
leonardo-racic <leonardo.racic.travail@gmail.com>
2025-01-02 19:19:23 +0100

    MainMenu music
    - added music and corrected some things

--- Scripts/unique/MainMenu.cs
+++ Scripts/unique/MainMenu.cs
@@ -4,9 +4,12 @@ public partial class MainMenu : CanvasLayer
 {
 	[Export]
 	public Button[] buttons;
+	[Export]
+	public AudioStreamPlayer music;
 
 	public override void _Ready()
 	{
+		music.Finished += () => music.Play();
 		foreach (Button button in buttons)
 		{
 			string scenePath = $"res://Stages/{button.Name.ToString().TrimSuffix("Label").ToLower()}.tscn";
